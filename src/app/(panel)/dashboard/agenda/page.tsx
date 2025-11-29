5deaceb544dac328db4d2afbbd89923a813400a1
Sergio <sergio.costa_ads@outlook.com>
2025-11-29 16:42:31 +0000

    fix dashboard/agenda ajuste rota

--- src/app/(panel)/dashboard/agenda/page.tsx
+++ src/app/(panel)/dashboard/agenda/page.tsx
@@ -1,15 +1,15 @@
 // src/app/(panel)/dashboard/agendaAdmin/page.tsx
 import getSession from "@/lib/getSesstion";
 import { redirect } from "next/navigation";
+import AgendaDoDiaPage from "./_components/agenda";
 
-export default async function AgendaDoDiaPage() {
+export default async function AgendaPage() {
 
   const session = await getSession();
 
   if (!session) {
     redirect("/");
   }
-
 
   return (
     <AgendaDoDiaPage />
